991b954ba2de354fe971cd893b7259e8ddb5b2e3
Wahid Shaikh <wahidxzzz@gmail.com>
2021-02-05 17:11:32 +0530

    date picker done

--- js/custom.js
+++ js/custom.js
@@ -16,387 +16,415 @@
 
 ******************************/
 
-$(document).ready(function()
-{
-	"use strict";
+$(document).ready(function () {
+  "use strict";
 
-	/* 
+  /* 
 
 	1. Vars and Inits
 
 	*/
+  $(function () {
+    $(".t-datepicker").tDatePicker({
+      // auto close after selection
+      autoClose: false,
 
-	var menu = $('.menu');
-	var menuActive = false;
-	var header = $('.header');
-	var searchActive = false;
+      //   // animation speed in milliseconds
+      //   durationArrowTop: 200,
 
-	setHeader();
+      //   // the number of calendars
+      //   numCalendar: 2,
 
-	$(window).on('resize', function()
-	{
-		setHeader();
-	});
+      // localization
+      titleCheckIn: "Check In",
+      titleCheckOut: "Check Out",
+      titleToday: "Today",
+      titleDateRange: "night",
+      titleDateRanges: "nights",
+      titleDays: ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
+      titleMonths: [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "Septemper",
+        "October",
+        "November",
+        "December",
+      ],
 
-	$(document).on('scroll', function()
-	{
-		setHeader();
-	});
+      //   // the max length of the title
+      //   titleMonthsLimitShow: 3,
 
-	initHomeSlider();
-	initMenu();
-	initSearch();
-	initCtaSlider();
-	initTestSlider();
-	initSearchForm();
+      //   // replace moth names
+      //   replaceTitleMonths: null,
 
-	/* 
+      //   // e.g. 'dd-mm-yy'
+      //   showDateTheme: null,
+
+      //   // icon options
+      //   iconArrowTop: true,
+      //   iconDate: "&#x279C;",
+      //   arrowPrev: "&#x276E;",
+      //   arrowNext: "&#x276F;",
+      //   // https://fontawesome.com/v4.7.0/icons/
+      //   // iconDate: '<i class="li-calendar-empty"></i><i class="li-arrow-right"></i>',
+      //   // arrowPrev: '<i class="fa fa-chevron-left"></i>',
+      //   // arrowNext: '<i class="fa fa-chevron-right"></i>',
+
+      //   // shows today title
+      //   toDayShowTitle: true,
+
+      //   // showss dange range title
+      //   dateRangesShowTitle: true,
+
+      //   // highlights today
+      //   toDayHighlighted: false,
+
+      //   // highlights next day
+      //   nextDayHighlighted: false,
+
+      //   // an array of days
+      //   daysOfWeekHighlighted: [0, 6],
+
+      //   // custom date format
+      //   formatDate: "yyyy-mm-dd",
+
+      //   // dateCheckIn: '25/06/2018',  // DD/MM/YY
+      //   // dateCheckOut: '26/06/2018', // DD/MM/YY
+      //   dateCheckIn: null,
+      //   dateCheckOut: null,
+      //   startDate: null,
+      //   endDate: null,
+
+      //   // limits the number of months
+      //   limitPrevMonth: 0,
+      //   limitNextMonth: 11,
+
+      //   // limits the number of days
+      //   limitDateRanges: 31,
+
+      //   // true -> full days || false - 1 day
+      //   showFullDateRanges: false,
+
+      //   // DATA HOLIDAYS
+      //   // Data holidays
+      //   fnDataEvent: null,
+    });
+  });
+
+  var menu = $(".menu");
+  var menuActive = false;
+  var header = $(".header");
+  var searchActive = false;
+
+  setHeader();
+
+  $(window).on("resize", function () {
+    setHeader();
+  });
+
+  $(document).on("scroll", function () {
+    setHeader();
+  });
+
+  initHomeSlider();
+  initMenu();
+  initSearch();
+  initCtaSlider();
+  initTestSlider();
+  initSearchForm();
+
+  /* 
 
 	2. Set Header
 
 	*/
 
-	function setHeader()
-	{
-		if(window.innerWidth < 992)
-		{
-			if($(window).scrollTop() > 100)
-			{
-				header.addClass('scrolled');
-			}
-			else
-			{
-				header.removeClass('scrolled');
-			}
-		}
-		else
-		{
-			if($(window).scrollTop() > 100)
-			{
-				header.addClass('scrolled');
-			}
-			else
-			{
-				header.removeClass('scrolled');
-			}
-		}
-		if(window.innerWidth > 991 && menuActive)
-		{
-			closeMenu();
-		}
-	}
+  function setHeader() {
+    if (window.innerWidth < 992) {
+      if ($(window).scrollTop() > 100) {
+        header.addClass("scrolled");
+      } else {
+        header.removeClass("scrolled");
+      }
+    } else {
+      if ($(window).scrollTop() > 100) {
+        header.addClass("scrolled");
+      } else {
+        header.removeClass("scrolled");
+      }
+    }
+    if (window.innerWidth > 991 && menuActive) {
+      closeMenu();
+    }
+  }
 
-	/* 
+  /* 
 
 	3. Init Home Slider
 
 	*/
 
-	function initHomeSlider()
-	{
-		if($('.home_slider').length)
-		{
-			var homeSlider = $('.home_slider');
+  function initHomeSlider() {
+    if ($(".home_slider").length) {
+      var homeSlider = $(".home_slider");
 
-			homeSlider.owlCarousel(
-			{
-				items:1,
-				loop:true,
-				autoplay:false,
-				smartSpeed:1200,
-				dotsContainer:'main_slider_custom_dots'
-			});
+      homeSlider.owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: false,
+        smartSpeed: 1200,
+        dotsContainer: "main_slider_custom_dots",
+      });
 
-			/* Custom nav events */
-			if($('.home_slider_prev').length)
-			{
-				var prev = $('.home_slider_prev');
+      /* Custom nav events */
+      if ($(".home_slider_prev").length) {
+        var prev = $(".home_slider_prev");
 
-				prev.on('click', function()
-				{
-					homeSlider.trigger('prev.owl.carousel');
-				});
-			}
+        prev.on("click", function () {
+          homeSlider.trigger("prev.owl.carousel");
+        });
+      }
 
-			if($('.home_slider_next').length)
-			{
-				var next = $('.home_slider_next');
+      if ($(".home_slider_next").length) {
+        var next = $(".home_slider_next");
 
-				next.on('click', function()
-				{
-					homeSlider.trigger('next.owl.carousel');
-				});
-			}
+        next.on("click", function () {
+          homeSlider.trigger("next.owl.carousel");
+        });
+      }
 
-			/* Custom dots events */
-			if($('.home_slider_custom_dot').length)
-			{
-				$('.home_slider_custom_dot').on('click', function()
-				{
-					$('.home_slider_custom_dot').removeClass('active');
-					$(this).addClass('active');
-					homeSlider.trigger('to.owl.carousel', [$(this).index(), 300]);
-				});
-			}
+      /* Custom dots events */
+      if ($(".home_slider_custom_dot").length) {
+        $(".home_slider_custom_dot").on("click", function () {
+          $(".home_slider_custom_dot").removeClass("active");
+          $(this).addClass("active");
+          homeSlider.trigger("to.owl.carousel", [$(this).index(), 300]);
+        });
+      }
 
-			/* Change active class for dots when slide changes by nav or touch */
-			homeSlider.on('changed.owl.carousel', function(event)
-			{
-				$('.home_slider_custom_dot').removeClass('active');
-				$('.home_slider_custom_dots li').eq(event.page.index).addClass('active');
-			});	
+      /* Change active class for dots when slide changes by nav or touch */
+      homeSlider.on("changed.owl.carousel", function (event) {
+        $(".home_slider_custom_dot").removeClass("active");
+        $(".home_slider_custom_dots li")
+          .eq(event.page.index)
+          .addClass("active");
+      });
 
-			// add animate.css class(es) to the elements to be animated
-			function setAnimation ( _elem, _InOut )
-			{
-				// Store all animationend event name in a string.
-				// cf animate.css documentation
-				var animationEndEvent = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+      // add animate.css class(es) to the elements to be animated
+      function setAnimation(_elem, _InOut) {
+        // Store all animationend event name in a string.
+        // cf animate.css documentation
+        var animationEndEvent =
+          "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
 
-				_elem.each ( function ()
-				{
-					var $elem = $(this);
-					var $animationType = 'animated ' + $elem.data( 'animation-' + _InOut );
+        _elem.each(function () {
+          var $elem = $(this);
+          var $animationType = "animated " + $elem.data("animation-" + _InOut);
 
-					$elem.addClass($animationType).one(animationEndEvent, function ()
-					{
-						$elem.removeClass($animationType); // remove animate.css Class at the end of the animations
-					});
-				});
-			}
+          $elem.addClass($animationType).one(animationEndEvent, function () {
+            $elem.removeClass($animationType); // remove animate.css Class at the end of the animations
+          });
+        });
+      }
 
-			// Fired before current slide change
-			homeSlider.on('change.owl.carousel', function(event)
-			{
-				var $currentItem = $('.home_slider_item', homeSlider).eq(event.item.index);
-				var $elemsToanim = $currentItem.find("[data-animation-out]");
-				setAnimation ($elemsToanim, 'out');
-			});
+      // Fired before current slide change
+      homeSlider.on("change.owl.carousel", function (event) {
+        var $currentItem = $(".home_slider_item", homeSlider).eq(
+          event.item.index
+        );
+        var $elemsToanim = $currentItem.find("[data-animation-out]");
+        setAnimation($elemsToanim, "out");
+      });
 
-			// Fired after current slide has been changed
-			homeSlider.on('changed.owl.carousel', function(event)
-			{
-				var $currentItem = $('.home_slider_item', homeSlider).eq(event.item.index);
-				var $elemsToanim = $currentItem.find("[data-animation-in]");
-				setAnimation ($elemsToanim, 'in');
-			})
-		}
-	}
+      // Fired after current slide has been changed
+      homeSlider.on("changed.owl.carousel", function (event) {
+        var $currentItem = $(".home_slider_item", homeSlider).eq(
+          event.item.index
+        );
+        var $elemsToanim = $currentItem.find("[data-animation-in]");
+        setAnimation($elemsToanim, "in");
+      });
+    }
+  }
 
-	/* 
+  /* 
 
 	4. Init Menu
 
 	*/
 
-	function initMenu()
-	{
-		if($('.hamburger').length && $('.menu').length)
-		{
-			var hamb = $('.hamburger');
-			var close = $('.menu_close_container');
+  function initMenu() {
+    if ($(".hamburger").length && $(".menu").length) {
+      var hamb = $(".hamburger");
+      var close = $(".menu_close_container");
 
-			hamb.on('click', function()
-			{
-				if(!menuActive)
-				{
-					openMenu();
-				}
-				else
-				{
-					closeMenu();
-				}
-			});
+      hamb.on("click", function () {
+        if (!menuActive) {
+          openMenu();
+        } else {
+          closeMenu();
+        }
+      });
 
-			close.on('click', function()
-			{
-				if(!menuActive)
-				{
-					openMenu();
-				}
-				else
-				{
-					closeMenu();
-				}
-			});
+      close.on("click", function () {
+        if (!menuActive) {
+          openMenu();
+        } else {
+          closeMenu();
+        }
+      });
+    }
+  }
 
-	
-		}
-	}
+  function openMenu() {
+    menu.addClass("active");
+    menuActive = true;
+  }
 
-	function openMenu()
-	{
-		menu.addClass('active');
-		menuActive = true;
-	}
+  function closeMenu() {
+    menu.removeClass("active");
+    menuActive = false;
+  }
 
-	function closeMenu()
-	{
-		menu.removeClass('active');
-		menuActive = false;
-	}
-
-	/* 
+  /* 
 
 	5. Init Search
 
 	*/
 
-	function initSearch()
-	{
-		if($('.search_tab').length)
-		{
-			$('.search_tab').on('click', function()
-			{
-				$('.search_tab').removeClass('active');
-				$(this).addClass('active');
-				var clickedIndex = $('.search_tab').index(this);
+  function initSearch() {
+    if ($(".search_tab").length) {
+      $(".search_tab").on("click", function () {
+        $(".search_tab").removeClass("active");
+        $(this).addClass("active");
+        var clickedIndex = $(".search_tab").index(this);
 
-				var panels = $('.search_panel');
-				panels.removeClass('active');
-				$(panels[clickedIndex]).addClass('active');
-			});
-		}
-	}
+        var panels = $(".search_panel");
+        panels.removeClass("active");
+        $(panels[clickedIndex]).addClass("active");
+      });
+    }
+  }
 
-	/* 
+  /* 
 
 	6. Init CTA Slider
 
 	*/
 
-	function initCtaSlider()
-	{
-		if($('.cta_slider').length)
-		{
-			var ctaSlider = $('.cta_slider');
+  function initCtaSlider() {
+    if ($(".cta_slider").length) {
+      var ctaSlider = $(".cta_slider");
 
-			ctaSlider.owlCarousel(
-			{
-				items:1,
-				loop:true,
-				autoplay:false,
-				nav:false,
-				dots:false,
-				smartSpeed:1200
-			});
+      ctaSlider.owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: false,
+        nav: false,
+        dots: false,
+        smartSpeed: 1200,
+      });
 
-			/* Custom nav events */
-			if($('.cta_slider_prev').length)
-			{
-				var prev = $('.cta_slider_prev');
+      /* Custom nav events */
+      if ($(".cta_slider_prev").length) {
+        var prev = $(".cta_slider_prev");
 
-				prev.on('click', function()
-				{
-					ctaSlider.trigger('prev.owl.carousel');
-				});
-			}
+        prev.on("click", function () {
+          ctaSlider.trigger("prev.owl.carousel");
+        });
+      }
 
-			if($('.cta_slider_next').length)
-			{
-				var next = $('.cta_slider_next');
+      if ($(".cta_slider_next").length) {
+        var next = $(".cta_slider_next");
 
-				next.on('click', function()
-				{
-					ctaSlider.trigger('next.owl.carousel');
-				});
-			}
-		}
-	}
+        next.on("click", function () {
+          ctaSlider.trigger("next.owl.carousel");
+        });
+      }
+    }
+  }
 
-	/* 
+  /* 
 
 	7. Init Testimonials Slider
 
 	*/
 
-	function initTestSlider()
-	{
-		if($('.test_slider').length)
-		{
-			var testSlider = $('.test_slider');
+  function initTestSlider() {
+    if ($(".test_slider").length) {
+      var testSlider = $(".test_slider");
 
-			testSlider.owlCarousel(
-			{
-				loop:true,
-				nav:false,
-				dots:false,
-				smartSpeed:1200,
-				margin:30,
-				responsive:
-				{
-					0:{items:1},
-					480:{items:1},
-					768:{items:2},
-					992:{items:3}
-				}
-			});
+      testSlider.owlCarousel({
+        loop: true,
+        nav: false,
+        dots: false,
+        smartSpeed: 1200,
+        margin: 30,
+        responsive: {
+          0: { items: 1 },
+          480: { items: 1 },
+          768: { items: 2 },
+          992: { items: 3 },
+        },
+      });
 
-			/* Custom nav events */
-			if($('.test_slider_prev').length)
-			{
-				var prev = $('.test_slider_prev');
+      /* Custom nav events */
+      if ($(".test_slider_prev").length) {
+        var prev = $(".test_slider_prev");
 
-				prev.on('click', function()
-				{
-					testSlider.trigger('prev.owl.carousel');
-				});
-			}
+        prev.on("click", function () {
+          testSlider.trigger("prev.owl.carousel");
+        });
+      }
 
-			if($('.test_slider_next').length)
-			{
-				var next = $('.test_slider_next');
+      if ($(".test_slider_next").length) {
+        var next = $(".test_slider_next");
 
-				next.on('click', function()
-				{
-					testSlider.trigger('next.owl.carousel');
-				});
-			}
-		}
-	}
+        next.on("click", function () {
+          testSlider.trigger("next.owl.carousel");
+        });
+      }
+    }
+  }
 
-	/* 
+  /* 
 
 	8. Init Search Form
 
 	*/
 
-	function initSearchForm()
-	{
-		if($('.search_form').length)
-		{
-			var searchForm = $('.search_form');
-			var searchInput = $('.search_content_input');
-			var searchButton = $('.content_search');
+  function initSearchForm() {
+    if ($(".search_form").length) {
+      var searchForm = $(".search_form");
+      var searchInput = $(".search_content_input");
+      var searchButton = $(".content_search");
 
-			searchButton.on('click', function(event)
-			{
-				event.stopPropagation();
+      searchButton.on("click", function (event) {
+        event.stopPropagation();
 
-				if(!searchActive)
-				{
-					searchForm.addClass('active');
-					searchActive = true;
+        if (!searchActive) {
+          searchForm.addClass("active");
+          searchActive = true;
 
-					$(document).one('click', function closeForm(e)
-					{
-						if($(e.target).hasClass('search_content_input'))
-						{
-							$(document).one('click', closeForm);
-						}
-						else
-						{
-							searchForm.removeClass('active');
-							searchActive = false;
-						}
-					});
-				}
-				else
-				{
-					searchForm.removeClass('active');
-					searchActive = false;
-				}
-			});	
-		}
-	}
+          $(document).one("click", function closeForm(e) {
+            if ($(e.target).hasClass("search_content_input")) {
+              $(document).one("click", closeForm);
+            } else {
+              searchForm.removeClass("active");
+              searchActive = false;
+            }
+          });
+        } else {
+          searchForm.removeClass("active");
+          searchActive = false;
+        }
+      });
+    }
+  }
 });
